102bb4e16a9347116586ee86ee5a5e4b9569347f
Sebastian Reinhard <sebastian@sebbert.me>
2023-03-19 23:28:28 +0100

    Tweak phase mod parameters, plot phase mod oscillator

--- Vsts/Vectron/VectronEditor.h
+++ Vsts/Vectron/VectronEditor.h
@@ -4,13 +4,25 @@
 #include <WaveSabreVstLib.h>
 using namespace WaveSabreVstLib;
 
+
+#include "VectronVst.h"
+
 class VectronEditor : public VstEditor
 {
 public:
-	VectronEditor(AudioEffect *audioEffect);
+	VectronEditor(VectronVst *vst);
 	virtual ~VectronEditor();
 
 	virtual void Open();
+	virtual void Close();
+
+	virtual void setParameter(VstInt32 index, float value);
+	virtual void valueChanged(CControl *control);
+
+protected:
+	Vectron *vectron;
+
+	class PhaseModPlotView *phaseModPlot;
 };
 
 #endif
