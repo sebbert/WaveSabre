024777f1dddb3c009bedc47d8c03993515a4d280
emil.sandsto <emil.sandsto@gmail.com>
2023-10-29 13:05:26 +0100

    Reduce size more

--- WaveSabreCore/src/Main.cpp
+++ WaveSabreCore/src/Main.cpp
@@ -1,13 +1,6 @@
 #include <WaveSabreCore.h>
-#include <emscripten/console.h>
 
 int main() {
-  char *test = new char[256];
-  test[0] = 'H';
-  test[1] = 'e';
-  test[2] = '\0';
-  emscripten_console_log(test);
-
   new WaveSabreCore::Falcon();
   new WaveSabreCore::Slaughter();
   new WaveSabreCore::Scissor();
--- WaveSabreCore/src/NewImpl.cpp
+++ WaveSabreCore/src/NewImpl.cpp
@@ -25,3 +25,16 @@ void operator delete[](void *p) noexcept
 {
   // TODO: implement
 }
+
+#include "cxxabi.h"
+extern "C" {
+_LIBCXXABI_FUNC_VIS _LIBCXXABI_NORETURN
+void __cxa_pure_virtual(void) {
+  // abort_message("Pure virtual function called!");
+}
+
+_LIBCXXABI_FUNC_VIS _LIBCXXABI_NORETURN
+void __cxa_deleted_virtual(void) {
+  // abort_message("Deleted virtual function called!");
+}
+}
