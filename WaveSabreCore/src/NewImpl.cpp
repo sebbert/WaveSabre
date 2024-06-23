ac6098e587fc6311ceb2a6068bfaf14a7ec912f6
Sebastian Reinhard <sebastian@sebbert.me>
2024-06-23 14:05:52 +0200

    Clean up NewImpl

--- WaveSabreCore/src/NewImpl.cpp
+++ WaveSabreCore/src/NewImpl.cpp
@@ -1,40 +1,48 @@
 #include <new>
 
 // Simple bump alloc. which leaks memory
-char buffer[1024*1024]; // 20 mb buffer for heap
-int off;
+static char buffer[1024*1024]; // 20 mb buffer for heap
+static int offset;
 
-void * operator new(std::size_t n)
-{
-  auto startOfBlock = buffer + off;
-  off += n;
-  return (void*)startOfBlock;
+void * bumpAlloc(std::size_t size)
+{	
+	auto startOfBlock = buffer + offset;
+	offset += size;
+	return (void*)startOfBlock;
 }
-void *operator new[](std::size_t s)
+
+void * operator new(std::size_t size)
 {
-  auto startOfBlock = buffer + off;
-  off += s;
-  return (void*)startOfBlock;
+	return bumpAlloc(size);
+}
+
+void * operator new[](std::size_t size)
+{
+	return bumpAlloc(size);
 }
 
 void operator delete(void * p) noexcept
 {
-  //...
+	//...
 }
+
 void operator delete[](void *p) noexcept
 {
-  // TODO: implement
+	// TODO: implement
 }
 
 #include "cxxabi.h"
-extern "C" {
-_LIBCXXABI_FUNC_VIS _LIBCXXABI_NORETURN
-void __cxa_pure_virtual(void) {
-  // abort_message("Pure virtual function called!");
-}
+extern "C"
+{
+	_LIBCXXABI_FUNC_VIS _LIBCXXABI_NORETURN
+	void __cxa_pure_virtual(void)
+	{
+		// abort_message("Pure virtual function called!");
+	}
 
-_LIBCXXABI_FUNC_VIS _LIBCXXABI_NORETURN
-void __cxa_deleted_virtual(void) {
-  // abort_message("Deleted virtual function called!");
-}
+	_LIBCXXABI_FUNC_VIS _LIBCXXABI_NORETURN
+	void __cxa_deleted_virtual(void)
+	{
+		// abort_message("Deleted virtual function called!");
+	}
 }
